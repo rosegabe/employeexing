e67cdda1d0fc0a16b5fec946f46563c13236252d
rosegabe <gabrieldimi96@gmail.com>
2023-11-17 17:58:31 +0100

    more infos on employees

--- xing.py
+++ xing.py
@@ -87,7 +87,7 @@ def unauthenticated_call(company, domain, format_index, custom_email, csv_separa
 
     return data_list
 
-def get_xing_employees(company, amount, username, password, sort):
+def authenticated_call(args,custom_email):
     options = Options()
     options.add_argument('--headless')  # Run ChromeDriver in headless mode
     options.add_argument('--no-sandbox')
@@ -101,10 +101,10 @@ def get_xing_employees(company, amount, username, password, sort):
 
     # Find the username and password input fields and enter your credentials
     username_field = driver.find_element(By.ID, 'username')
-    username_field.send_keys(username)
+    username_field.send_keys(args.username)
 
     password_field = driver.find_element(By.ID, 'password')
-    password_field.send_keys(password)
+    password_field.send_keys(args.password)
 
     # Submit the login form
     password_field.send_keys(Keys.RETURN)
@@ -137,7 +137,7 @@ def get_xing_employees(company, amount, username, password, sort):
     company_data = {
         "operationName": "EntitySubpage",
         "variables": {
-            "id": company,
+            "id": args.company,
             "moduleType": "employees"
         },
         "query": "query EntitySubpage($id: SlugOrID!) {entityPageEX(id: $id) { ... on EntityPage {context { companyId }}}}"}
@@ -176,34 +176,38 @@ def get_xing_employees(company, amount, username, password, sort):
             json_response = employee_count_response.json()
             employee_count = json_response['data']['company']['employees']['total']
             print(f">>> {employee_count} employees have been found.")
-            if amount is None or amount >= 3000:
+
+            if args.amount is None or args.amount >= 3000:
                 if employee_count >= 3000:
-                    amount = 2999
+                    returnable_amount = 2999
                     print(f">>>XING allows only extracting up to 3000 datasets.")
                 else:
-                    amount = employee_count
+                    returnable_amount = employee_count
+            else:
+                returnable_amount = 2999
 
             # One could sort between LAST_NAME and CONNECTION_DEGREE
+
             employee_data = {
                 "operationName": "Employees",
                 "variables": {
                     "consumer": "",
                     "includeTotalQuery":True,
                     "id": company_id,
-                    "first": amount,
+                    "first": returnable_amount,
                     "query": {
                         "consumer": "web.entity_pages.employees_subpage",
-                        "sort": sort
+                        "sort": args.sort
                     }
                 },
-                "query": "query Employees($id: SlugOrID!, $first: Int, $after: String, $query: CompanyEmployeesQueryInput!, $consumer: String! = \"\", $includeTotalQuery: Boolean = false) {\n  company(id: $id) {\n    id\n    totalEmployees: employees(first: 0, query: {consumer: $consumer}) @include(if: $includeTotalQuery) {\n      total\n      __typename\n    }\n    employees(first: $first, after: $after, query: $query) {\n      total\n      edges { node {profileDetails {id firstName lastName displayName gender clickReasonProfileUrl(clickReasonId: CR_WEB_PUBLISHER_EMPLOYEES_MODULE){profileUrl} occupations {subline}}}}}}}"
+                "query": "query Employees($id: SlugOrID!, $first: Int, $after: String, $query: CompanyEmployeesQueryInput!, $consumer: String! = \"\", $includeTotalQuery: Boolean = true) {\n  company(id: $id) {\n    id\n    totalEmployees: employees(first: 0, query: {consumer: $consumer}) @include(if: $includeTotalQuery) {\n      total\n      __typename\n    }\n    employees(first: $first, after: $after, query: $query) {\n      total\n      edges {\n       node {\n         contactDistance {\n            distance\n            __typename\n          }\n          sharedContacts {\n            total\n            __typename\n          } \n profileDetails {\n id\n            firstName\n     lastName\n          displayName\n            gender\n            pageName\n            profileImage(size: SQUARE_256) {\n              url\n              __typename\n            }\n    clickReasonProfileUrl(clickReasonId: CR_WEB_PUBLISHER_EMPLOYEES_MODULE) {\n              profileUrl\n              __typename\n            }\n            userFlags {\n              displayFlag\n              __typename\n            }\n            occupations {\n     headline\n          subline\n              __typename\n            }\n location { city \n street\n  zip \n __typename}\n            __typename\n          }  \n          networkRelationship {\n          id\n            relationship\n            permissions\n            error\n            __typename\n          } \n          __typename\n        }\n        __typename\n      }\n      pageInfo {\n        endCursor\n        hasNextPage\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}"
             }
 
             employee_response = requests.post(api_url, headers=headers, json=employee_data)
 
             # Check the response status code
             if employee_response.status_code == 200:
-                return employee_response.json()
+                return authenticated_employee_parsing(employee_response.json(), args, custom_email, api_url, headers)
             else:
                 print(f">>> Employee request failed with status code {employee_response.status_code}")
                 sys.exit(0)
@@ -214,12 +218,13 @@ def get_xing_employees(company, amount, username, password, sort):
         print(f">>> Company request failed with status code {company_response.status_code}")
         sys.exit(0)
 
-def authenticated_call(employee_response, domain, format, custom_email, csv_separator, csv_header):
+def authenticated_employee_parsing(employee_response, args, custom_email, api_url, headers):
     data_list = []
+    
     employees = employee_response['data']['company']['employees']['edges']
 
-    if csv_header:
-        data_list.append([f"Email-address{csv_separator}Display-Name{csv_separator}Firstname{csv_separator}Lastname{csv_separator}Gender{csv_separator}Occupations{csv_separator}Profile-URL"])
+    if args.csv_header:
+        data_list.append([f"Email Address{args.csv_separator}Alternative Email Address{args.csv_separator}Display-Name{args.csv_separator}Firstname{args.csv_separator}Lastname{args.csv_separator}Gender{args.csv_separator}City{args.csv_separator}Street{args.csv_separator}ZIP{args.csv_separator}Mobile Number{args.csv_separator}Fax Number{args.csv_separator}Telephone number{args.csv_separator}Occupations{args.csv_separator}Profile-URL"])
 
     # Extract and print employee information
     for edge in employees:
@@ -234,16 +239,47 @@ def authenticated_call(employee_response, domain, format, custom_email, csv_sepa
         display_name = profile_details['displayName']
         gender = profile_details['gender']
 
+        city = profile_details['location']['city']
+        street = profile_details['location']['street']
+        xing_email_address = ""
+        mobile_number = ""
+        fax_number = ""
+        telephone_number = ""
+        zip_code = ""
+
+        if street:
+            contact_data = {
+                "operationName":"profileContactDetails",
+                "variables": {
+                    "profileId": profile_details['pageName']
+                },
+                "query":"query profileContactDetails($profileId: SlugOrID!) {\n  profileModules(id: $profileId) {\n    xingIdModule {\n      ...xingIdContactDetails\n      outdated\n      lastModified\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment xingIdContactDetails on XingIdModule {\n  contactDetails {\n    business {\n      address {\n        city\n        country {\n          countryCode\n          name: localizationValue\n          __typename\n        }\n        province {\n          id\n          canonicalName\n          name: localizationValue\n          __typename\n        }\n        street\n        zip\n        __typename\n      }\n      email\n      fax {\n        phoneNumber\n        __typename\n      }\n      mobile {\n        phoneNumber\n        __typename\n      }\n      phone {\n        phoneNumber\n        __typename\n      }\n      __typename\n    }\n    private {\n      address {\n        city\n        country {\n          countryCode\n          name: localizationValue\n          __typename\n        }\n        province {\n          id\n          canonicalName\n          name: localizationValue\n          __typename\n        }\n        street\n        zip\n        __typename\n      }\n      email\n      fax {\n        phoneNumber\n        __typename\n      }\n      mobile {\n        phoneNumber\n        __typename\n      }\n      phone {\n        phoneNumber\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n  __typename\n}"
+            }
+            contact_data_response = requests.post(api_url, headers=headers, json=contact_data)
+            
+            if contact_data_response.status_code == 200:
+                contact_data_response = contact_data_response.json()
+                business_data = contact_data_response['data']['profileModules']['xingIdModule']['contactDetails']['business']
+                mobile_number = business_data['mobile']['phoneNumber'] if business_data['mobile'] is not None else ""
+                fax_number = business_data['fax']['phoneNumber'] if business_data['fax'] is not None else ""
+                telephone_number = business_data['phone']['phoneNumber'] if business_data['phone'] is not None else ""
+                zip_code = business_data['address']['zip'] if  business_data['address'] is not None else ""
+                xing_email_address = business_data['email']
+            else:
+                print(f">>> Contact data request failed with status code {contact_data_response.status_code}")
+                sys.exit(0)
+            
+
         occupations_all = profile_details['occupations']
         occupations_list = [occupation['subline'] for occupation in occupations_all]
         occupations = '| '.join(occupations_list)
 
         if custom_email is None:
-            email_address = generate_email(first_name,last_name,domain,format)
+            email_address = generate_email(first_name,last_name,args.domain,args.format)
         else:
-            email_address = generate_email(first_name,last_name,domain,1,custom_email)
+            email_address = generate_email(first_name,last_name,args.domain,1,custom_email)
 
-        csv_entry = [f'{email_address}{csv_separator}{display_name}{csv_separator}{first_name}{csv_separator}{last_name}{csv_separator}{gender}{csv_separator}{occupations}{csv_separator}{profile_url}']
+        csv_entry = [f'{email_address}{args.csv_separator}{xing_email_address}{args.csv_separator}{display_name}{args.csv_separator}{first_name}{args.csv_separator}{last_name}{args.csv_separator}{gender}{args.csv_separator}{city}{args.csv_separator}{street}{args.csv_separator}{zip_code}{args.csv_separator}{mobile_number}{args.csv_separator}{fax_number}{args.csv_separator}{telephone_number}{args.csv_separator}{occupations}{args.csv_separator}{profile_url}']
         data_list.append(csv_entry)
 
     return data_list
@@ -287,8 +323,7 @@ if __name__ == '__main__':
         print(">>> No user credentials were provided, preceding with unauthenticated search, which returns up to thirty employees.")
         data_list = unauthenticated_call(args.company, args.domain, args.format, custom_email, args.csv_separator, args.csv_header)
     else:
-        employee_response = get_xing_employees(args.company, args.amount, args.username, args.password, args.sort)
-        data_list = authenticated_call(employee_response, args.domain, args.format, custom_email, args.csv_separator, args.csv_header)
+        data_list = authenticated_call(args, custom_email)
 
     with open(output_file, 'w') as f:
             writer = csv.writer(f)
